3f5cf14e5650a408583346a228243888c6d6e391
Haakon Ludvig Langeland Ervik <45243236+haakon-e@users.noreply.github.com>
2021-02-25 23:26:43 +0100

    initial commit

---
+++ Grids.jl
@@ -0,0 +1,90 @@
+module Grids
+#using LinearAlgebra
+#using Distributions
+#using Plots
+#using Statistics
+
+
+export Point, mean_point
+"""
+A `Point` is a pair of coordinates (x,y).
+"""
+struct Point{T<:Real}
+    x::T
+    y::T
+end
+
+# Basic operations on pairs of points: Compute the length between them and their midpoint.
+Base.length(p::Point, q::Point) = sqrt((p.x-q.x)^2 + (p.y-q.y)^2)
+mean_point(p::Point, q::Point) = Point((p.x+q.x)/2, (p.y+q.y)/2);
+
+export AbstractGrid, Grid, makeGrid
+
+abstract type AbstractGrid end
+
+"""
+A `Grid` is a regular 2D grid defined by an array of `nodes`, along with the spacing between the nodes
+determined by `Δx` and `Δy`. The nodes define the lower-left corners of the cells of the `Grid`.
+Associated to the `nodes` are `centers`, which are the coordinates of the cell centers for each grid point.
+For convencience, `xticks` and `yticks` store the x- and y- coordinates in separate arrays, like ticks
+on a plot. In addition, `nx` and `ny` is the number of cells along the x- and y-dimension.
+"""
+mutable struct Grid{I<:Integer,F<:Real} <: AbstractGrid
+    nodes::Array{Point{F},1}
+    centers::Array{Point{F},1}
+    Δx::F
+    Δy::F
+    xticks::Array{F}
+    yticks::Array{F}
+    nx::I
+    ny::I
+end
+
+function makeGrid(nx::I, ny::I, Δx::F, Δy::F) where {I<:Integer, F<:Real}
+    xticks = collect(0:Δx:nx*Δx)
+    yticks = collect(0:Δy:ny*Δy)
+    nodes = Point{Float64}[]
+    centers = Point{Float64}[]
+    for row in 1:nx
+        for col in 1:ny
+            x = xticks[row]; y = yticks[col]
+            push!(nodes, Point(x, y))
+            push!(centers, Point(x + Δx/2, y + Δy/2))
+        end
+    end
+    return Grid(nodes, centers, Δx, Δy, xticks, yticks, nx, ny)
+    #new(nodes,centers,Δx,Δy,xticks,yticks,nx,ny)
+end
+
+export bounding_box
+"""
+Compute the grid bounding box.
+Returns the minimum and maximum x- and y-coordinates of the grid.
+"""
+function bounding_box(grid::Grid)
+    xmin = minimum(grid.xticks); xmax = maximum(grid.xticks)
+    ymin = minimum(grid.yticks); ymax = maximum(grid.yticks)
+    return [xmin, ymin, xmax, ymax]
+end
+
+export grid_neighbors
+""""
+Returns a matrix where a non-zero value implies that the row (cell) and column (other cell) are neighbors.
+"""
+function grid_neighbors(grid::Grid)
+    c = grid.centers; nc = length(c)
+    neighbors = zeros(Int64, nc, nc)
+    for (idx, cell) in enumerate(c)
+        for odx in idx+1:nc  # Only compare points that haven't been compared before
+            ocell = c[odx];
+            xngb = (abs(cell.x - ocell.x) ≈ grid.Δx) & (cell.y ≈ ocell.y)
+            yngb = (abs(cell.y - ocell.y) ≈ grid.Δy) & (cell.x ≈ ocell.x)
+            if xngb | yngb
+                neighbors[idx, odx] = neighbors[odx, idx] = 1
+            end
+        end
+    end
+    neighbors
+end
+
+end  # module
---
+++ Rays.jl
@@ -0,0 +1,96 @@
+module Rays
+
+include("./Grids.jl")
+using .Grids
+
+export decompose_ray
+"""
+For each receiver, find the ray segments (and ids) through the cells
+"""
+function decompose_ray(
+        rec::Point{T},
+        src::Point{T},
+        grid::G,
+    ) where {T<:Real, G}
+    # Find coordinates of interesection with grid
+    xs = sort([src.x, rec.x])
+    ys = sort([src.y, rec.y])
+    xind = @. xs[1] < grid.xticks < xs[2]
+    yind = @. ys[1] < grid.yticks < ys[2]
+    cx = grid.xticks[xind]  # x ticks that ray passes through
+    cy = grid.yticks[yind]  # y ticks that ray passes through
+
+    # Compute angle of ray
+    θ = atan(rec.y - src.y, rec.x - src.x)
+
+    # Compute points of intersection with gridlines
+    cx_y = @. (cx - src.x) * tan(θ) + src.y  # y-coords of ray intersection with vertical gridlines
+    cy_x = @. (cy - src.y) / tan(θ) + src.x  # x-coords of ray intersection with horizontal gridlines
+
+    # Turn intersection coordinates into `Point`s
+    x_pts = vcat(cx,cy_x)
+    y_pts = vcat(cx_y,cy)
+    pts = Point{T}[]
+    for (x,y) in zip(x_pts,y_pts)
+        push!(pts,Point(x,y))
+    end
+
+    # Remove any points not within the domain
+    filter!(pt -> (
+            minimum(grid.xticks) <= pt.x <= maximum(grid.xticks)) && (
+            minimum(grid.yticks) <= pt.y <= maximum(grid.yticks)),
+        pts)
+
+    # Sort intersection points by distance to source
+    # (first point closest to source, last point closest to receiver)
+    sort!(pts, by=(p->length(p, src)))
+
+    # Add `src` to the end of the point list
+    push!(pts, rec)
+
+    # Compute lengths and center points of segments
+    lengths = T[]
+    centers = Point{T}[]
+    for i in 2:length(pts)
+        push!(lengths, length(pts[i-1], pts[i]))
+        push!(centers, mean_point(pts[i-1], pts[i]))
+    end
+
+    # For each line, find which Grid cell it belongs to
+    line_ind = Int64[]
+    for p in centers
+        found = false
+        for (ind, node) in pairs(grid.nodes)
+            cond = (node.x <= p.x) & (p.x < node.x + grid.Δx) & (node.y <= p.y) & (p.y < node.y + grid.Δy)
+            if cond
+                found = true
+                push!(line_ind, ind)
+                break
+            end
+        end
+    end
+    return lengths, line_ind
+end
+
+
+# Design matrix
+export distanceMatrix
+ν
+"""
+Construct matrix which describes the distance that each float (rows)
+passes through the grid boxes (columns).
+"""
+function distanceMatrix(
+        receivers::Array{Point{T},1},
+        src::Point{T},
+        grid::Grid,
+	) where {T<:Real}
+    D = zeros((length(receivers), grid.nx*grid.ny))
+    for (i, rec) in pairs(receivers)  # iterate over receivers
+        (lengths, inds) = decompose_ray(rec, src, grid)
+        D[i, inds] = lengths
+    end
+    D
+end
+
+end  # module
