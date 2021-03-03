ce6c87290cc7c9ef8e8d89b854a384e4bee5a785
Haakon Ludvig Langeland Ervik <45243236+haakon-e@users.noreply.github.com>
2021-03-03 10:19:39 +0100

    fix: update inversion procedure

--- src/Inversion.jl
+++ src/Inversion.jl
@@ -1,3 +1,16 @@
+"""
+Solve the inverse problem where an unknown state vector x is
+linearly related to a set of observations y through the relation
+	Ex + n = y
+where E is the design matrix and n is measurement noise not.
+The result is an application of the Gauss-Markov theorem which
+produces the values x̃ that minimizes the variance of all possible
+linear estimators.
+
+The implementation follows chapter 2.7.2 of
+Wunsch, 2009: Discrete Inverse and State Estimation Problems.
+Specifically, refer to Eqs. 2.400, 2.403, 2.405.
+"""
 module Inversion
 
 # Local packages
@@ -7,14 +20,21 @@ using ..Grids
 using LinearAlgebra
 
 # Export methods
-export Rxx, Ryy, Rnn, computeUncertaintyMatrix
+export Rxx, Ryy, Rnn 
+export computeUncertaintyMatrix, computeInversion
 
 # Methods
 """
     Rxx(grid::Grid, σ::T, λ::T) where T<:Real
 
-Compute the second-order moment of the physical variable x
-given correlation level σ and de
+Compute the prior covariance of the physical variable x
+given correlation level σ and decorrelation scale λ.
+
+Typically, this is the spatial correlation between
+locations in the grid space.
+Units:
+	- λ [length]
+	- σ [time / length]
 """
 function Rxx(grid::Grid, σ::T, λ::T) where T<:Real
     n = grid.nx*grid.ny; kernel = zeros(n,n)
@@ -26,17 +46,49 @@ function Rxx(grid::Grid, σ::T, λ::T) where T<:Real
     kernel
 end
 
-function Ryy(grid::Grid, σ::T) where T<:Real
+"""
+	Rnn(grid::Grid, σ::T) where T<:Real
+
+Compute the prior covariance of the data given correlation level σ.
+
+Typically, this is the position uncertainty of the floats.
+The correlation level σ is typically expressed as
+`pos. uncertainty` / `wave speed` = `travel time uncertainty` 
+Units:
+	- σ [time]
+"""
+function Rnn(grid::Grid, σ::T) where T<:Real
     σ^2 * I
 end
 
-function Rnn(E, rxx, ryy)
-    ryy - E * rxx * E'
+"""
+	Ryy(E, rxx, rnn)
+
+Compute the prior covariance of the measurements y given
+Rnn, Rxx, and E.
+"""
+function Ryy(E, rxx, rnn)
+    rnn + E * rxx * E'
 end
 
-function computeUncertaintyMatrix(E,rxx,rnn)
+"""
+	computeUncertaintyMatrix(E, rxx, rnn)
+
+Compute the uncertainty matrix for the linear relation
+	y = Ex + n,
+"""
+function computeUncertaintyMatrix(E, rxx, rnn)
     P = rxx - rxx * E' * inv(E * rxx * E' + rnn) * E * rxx
     return P
+end
+
+"""
+	computeInversion(E, rxx, rnn, y)
+
+Compute the solution x̃ of the inverse problem.
+"""
+function computeInversion(E, rxx, rnn, y)
+	rxx * E' * inv(E * rxx * E' + rnn) * y
 end
 
 
